452afe8123c954157aa1f64cb1ec32f137808f30
Evgeniya Malashenko <malashenko_genya@mail.ru>
2023-05-20 13:55:09 +0300

    Четвертое задание по объектам полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
+}
+let student1 = new Student("Полина", "женский", 23);
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (this.marks === undefined) {
+		return 0
+	} else {
+		this.marks.push(...marks);
+	}
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.getAverage = function() {
+	if (this.marks == undefined) {
+		return 0;
+	} else {
+		return this.marks.reduce((acc, mark, index, marks) => acc += mark / marks.length, 0);
+	}
 }
 
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
