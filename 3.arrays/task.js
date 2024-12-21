087f437b0a9a8b9ea8841b656d83cb08e884fcb2
Evgeniya Malashenko <malashenko_genya@mail.ru>
2024-12-21 23:44:42 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.every((el, idx) => arr1.length === arr2.length && el === arr2[idx]);  
+module.exports = {
+  compareArrays
+}
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    return users.filter(user => user.gender === gender).reduce((acc, user, index, users) => acc += user.age / users.length, 0 );
+    }
+module.exports = {
+  getUsersNamesInAgeRange
 }
